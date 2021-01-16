7c7673aebb0138ce673f5643fc17d54aa2d8aa30
irfanrmdni <irfanrmdni209@gmail.com>
2021-01-16 23:46:39 +0700

    mengatur file latihan javascript

--- PROJECT JAVASCRIPT/12-Responsive-Navbar/js/script.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector('input');
-const ul = document.querySelector('ul');
-
-window.addEventListener('click', function (e) {
-    if (e.target === input) {
-        ul.classList.toggle('slide');
-    }
-});
--- PROJECT JAVASCRIPT/css3-upskill-landing-page-main/css3-upskill-landing-page-main/README.md
+++
@@ -1,8 +0,0 @@
-# css3-upskill-landing-page
----
-Slicing Landing Page Up.Skill
----
-Slicing oleh Restu Kersana
----
-Design oleh Danu Prakoso
----
